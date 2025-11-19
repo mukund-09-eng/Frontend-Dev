e79e5639dea42c50d647c563b013b8bd4a4fbdb1
mukund-09-eng <mukundbansal000l@gmail.com>
2025-11-19 16:38:16 +0530

    2nd js-operator ques

---
+++ Desktop/Frontend-Dev/js-op2.js
@@ -0,0 +1,12 @@
+let product = " wireless headphones PRO ";
+
+let cleaned = product.trim().toLowerCase();
+
+let formatted = cleaned
+    .split(" ")
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ")
+    .replace("Pro", "Pro Edition");
+
+console.log("Cleaned Title:", formatted);
+console.log("Length:", formatted.length);
