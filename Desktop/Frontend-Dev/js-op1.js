d80145600613982bc2c41f2e6a90c7e5d0e0ffe6
mukund-09-eng <mukundbansal000l@gmail.com>
2025-11-19 16:38:05 +0530

    1st js-operator ques

---
+++ Desktop/Frontend-Dev/js-op1.js
@@ -0,0 +1,14 @@
+let bonus = 5000;
+
+function calculateSalary(isPermanent) {
+    let salary = 40000;
+    if (isPermanent) {
+        salary += bonus;
+    }
+    console.log("Total Salary:", salary);
+}
+
+calculateSalary(true);
+calculateSalary(false);
+
+console.log("Global Bonus Still:", bonus);
