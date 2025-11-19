b9bc8a27c0d17514ed29731ad6cc72bcfdf50326
mukund-09-eng <mukundbansal000l@gmail.com>
2025-11-19 16:39:20 +0530

    8th js-operator ques

---
+++ Desktop/Frontend-Dev/js-op8.js
@@ -0,0 +1,27 @@
+const cart = [
+    { item: "Laptop", category: "electronics", price: 45000 },
+    { item: "Shoes", category: "fashion", price: 2500 },
+    { item: "Book", category: "education", price: 600 }
+];
+
+let total = 0;
+
+for (let product of cart) {
+    let price = product.price;
+
+    if (product.category === "electronics") {
+        price -= price * 0.10;
+    } else if (product.category === "fashion") {
+        price -= price * 0.05;
+    }
+
+    total += price;
+}
+
+if (total > 50000) {
+    total -= total * 0.05;
+}
+
+let finalTotal = cart.reduce((sum, p) => sum + p.price, 0);
+
+console.log("Final Payable Amount:", Math.round(total));
