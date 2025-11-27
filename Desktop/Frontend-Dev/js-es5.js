125146170b49c568e5b575c2062d85d667433c19
mukund-09-eng <mukundbansal000l@gmail.com>
2025-11-27 11:49:04 +0530

    5th es-features ques

---
+++ Desktop/Frontend-Dev/js-es5.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const announce = () => console.log("Game started");
+const startGame = () => console.log(status);
+
+var score = 50;
+let status = "ready";
+
+console.log(score);
+announce();
+startGame();
